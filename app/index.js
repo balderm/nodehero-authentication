6fe9ce04f80935effd17b0072d0973b46bce68a0
balderm <black7b@gmail.com>
2017-02-20 18:02:07 +0100

    first commit
    building custom app on top of pre-existing example

--- app/index.js
+++ app/index.js
@@ -1,33 +1,37 @@
 const path = require('path')
-
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const session = require('express-session')
-const RedisStore = require('connect-redis')(session)
+const MySqlStore = require('express-mysql-session')(session)
 
-const config = require('../config')
 const app = express()
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-
+// load .env config file into env
+require('dotenv').config()
+// init bodyParser
+app.use(bodyParser.urlencoded({extended: false}))
+// init auth
 require('./authentication').init(app)
-
+// configure session to be stored inside MySQL
 app.use(session({
-  store: new RedisStore({
-    url: config.redisStore.url
+  store: new MySqlStore({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+//    , createDatabaseTable: true
   }),
-  secret: config.redisStore.secret,
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }))
-
+// init passport session manager
 app.use(passport.initialize())
 app.use(passport.session())
-
+// init express-handlebars integration
 app.engine('.hbs', exphbs({
   defaultLayout: 'layout',
   extname: '.hbs',
@@ -37,7 +41,9 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname))
-
+// expose static content folder
+app.use('/', express.static('public'))
+// init modules
 require('./user').init(app)
 require('./note').init(app)
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 const port = process.env.PORT || 3000
 
+// start server
 app.listen(port, function (err) {
   if (err) {
     throw err
